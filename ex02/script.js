10e8339dc49baf302bb63324b25d827b44584ef6
Lucas Chagas Magno <109705062+lucascmagno@users.noreply.github.com>
2023-05-16 14:46:43 -0300

    funcao de Limpar a tela criada

--- ex02/script.js
+++ ex02/script.js
@@ -2,9 +2,18 @@ function subtotal1(){
     var quant1 = parseFloat(document.getElementById("quant1").value)
     var preco1 = parseFloat(document.getElementById("preco1").value)
     var result = document.getElementById("subtotal1");
-    var total1 = quant1 * preco1;
+    var total1 = parseFloat(quant1) * parseFloat(preco1);
 
-    result.innerHTML= total1;
+    result.innerHTML = total1;
+}
+
+function subtotal2(){
+    var quant2 = parseFloat(document.getElementById("quant2").value)
+    var preco2 = parseFloat(document.getElementById("preco2").value)
+    var result = document.getElementById("subtotal2");
+    var total2 = quant2 * preco2;
+
+    result.innerHTML= total2;
 }
 
 function calcularTotal(){
@@ -35,4 +44,14 @@ function calcularTotal(){
     }else{
         result.innerHTML = total_geral ;
     }
+}
+
+function limparComanda(){
+    var cont = 1;
+    for(cont; cont <= 5; cont++){
+        document.getElementById("quant"+cont).value = '0'
+        document.getElementById("preco"+cont).value = '0'
+        document.getElementById("subtotal"+cont).value = '0'
+    }
+    document.getElementsByTagName("select").innerHTML = '-'
 }
